c919407aa48c50b42427f39056902d49ec1c7e07
Enn-Riiko Talivere <etaliver@tktk.ee>
2021-10-20 19:19:32 +0300

    Control Flow

---
+++ DrivingLisence/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DrivingLisence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm küsib kasutaja sünniaastat.
+            //Programm arvutab kasutaja vanuse.
+            //Kui kasutaja on noorem kui 18, siis programm kuvab konsooli
+            //"Oled liiga noor, et juhilube saada";
+            //Kui kasutaja on vanem kui 18, siis programm kuvab konsooli
+            //"Oled piisavalt vana, et juhilube saada."
+            //Kui kasutaja on 18, siis programm kuvab konsooli
+            //"Palju õnne! Nüüd sa saad juhilube taotleda"
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            if (userAge < 18)
+            {
+                Console.WriteLine("Oled liiga noor, et juhilube saada.");
+            }
+            else if (userAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhilube saada.");
+            }
+            else
+            {
+                Console.WriteLine("Palju õnne! Nüüd sa saad juhilube taotleda.");
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm küsib kasutaja vanust;
+            //Programm arvutab kasutaja sünniaasta;
+            //Programm kuvab sünniaasta konsooli;
+            //"Oled sündinud aastal {yearOfBirth}"
+
+            Console.WriteLine("Palun, sisesta oma vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - userAge;
+
+            Console.WriteLine($"Oled sündinud aastal {yearOfBirth}.");
+        }
+    }
+}
